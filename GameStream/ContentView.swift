fe5e6f9337aff2cfd271851fc9aec83bda21b172
Mich DMark <michdmark@gmail.com>
2022-10-22 01:30:05 -0500

    cree un navigation link de la pantalla de inicio de sesion a una pantalla de home y disene un tabview en la ventana de home

--- GameStream/ContentView.swift
+++ GameStream/ContentView.swift
@@ -9,18 +9,26 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ZStack
+        
+        NavigationView
         {
-            Spacer()
-            Color("Marine").ignoresSafeArea()
-            VStack
+            ZStack
             {
-                Image("AppLogo").resizable().aspectRatio(contentMode: .fit).frame(width: 250).padding(.bottom, 42)
-                
-                InicioSesionYRegistro()
-                
-            }
+                Spacer()
+                Color("Marine").ignoresSafeArea()
+                VStack
+                {
+                    Image("AppLogo").resizable().aspectRatio(contentMode: .fit).frame(width: 250).padding(.bottom, 42)
+                    
+                    InicioSesionYRegistro()
+                    
+                }
+            }.navigationBarHidden(true)
+            
+            
         }
+        
+        
     }
 }
 
@@ -61,12 +69,12 @@ struct InicioSesionYRegistro: View {
         }
     }
 }
-
 struct InicioSesionView: View
 {
     @State var correo: String = ""
     @State var password: String = ""
     @State var passwordVisible: Bool = false
+    @State var isPantallaHomeActive: Bool = false
     
     var body: some View
     {
@@ -133,7 +141,7 @@ struct InicioSesionView: View
                     .foregroundColor(Color("Dark-Cian"))
                     .padding(.bottom, 70)
                 
-                Button {IniciarSesion()}
+                Button {iniciarSesion() }
                 label:
                 {
                     Text("INICIAR SESION").fontWeight(.bold)
@@ -149,7 +157,6 @@ struct InicioSesionView: View
                     .padding(.vertical, 30)
                     .frame(maxWidth: .infinity, alignment: .center)
                     
-                
                 HStack
                 {
                     Button {InicioSesionFacebook()}
@@ -189,8 +196,16 @@ struct InicioSesionView: View
                 
                 
             }.padding(.horizontal, 77)
+            
+            NavigationLink(
+                destination: Home(),
+                isActive: $isPantallaHomeActive,
+                label: {EmptyView()})
         }
     }
+    
+    
+    func iniciarSesion(){isPantallaHomeActive = true}
 }
 
 struct RegistroView: View
@@ -415,9 +430,12 @@ struct ContentView_Previews: PreviewProvider {
     }
 }
 
-func IniciarSesion(){
-    print("Estoy inciando sesion")
-}
+
+//intento para sacar la funcion que manda a home fuera del struct inicio de sesion
+func iniciarSesion2()
+{ InicioSesionView().isPantallaHomeActive = true }
+ 
+
 
 func InicioSesionFacebook(){
     print("Estoy inciando sesion con facebook")
@@ -442,3 +460,5 @@ func RegistroConFacebook(){
 func RegistroConTwitter(){
     print("Estoy registrandome con twitter")
 }
+
+
